8c6f7dfca363a57ec8d5fb87cb9d8b12c62c8d2f
Paul Shao <paulshaoyuqiao1@berkeley.edu>
2019-01-14 23:29:49 -0500

    Major Updates: Supported Multiple File Submission/Autograding, Detect for Missing Files

--- app/controllers/autograder_controller.rb
+++ app/controllers/autograder_controller.rb
@@ -1,12 +1,27 @@
+require 'set'
+
 class AutograderController < ApplicationController
+    @@files_by_week = {"week1" => ["age_in_seconds.rb", "author_age.rb", "hours_in_year.rb", "minutes_in_decade.rb","bigger_better_favorite_number.rb", "full_name_greeting.rb", "angry_boss.rb", "table_of_contents.rb"],"week2" =>["99_bob.rb", "deaf_grandma.rb", "deaf_grandma_extended.rb", "leap_year_counter.rb", "array_sort.rb", "table_of_contents.rb"], "week3" => ["ask.rb", "old_roman_numerals.rb", "new_roman_numerals.rb"]}
     def import
-        code_file = params[:file]
-        script_path = code_file.path
-        output = `ls`
-        load script_path
-        value = `ruby -r "./E98/execute.rb" -e "Exec.run_all_test '#{script_path}'"`
-        @result_array_disp = value.split("\n")
-        @file_name = code_file.original_filename
+        filenames = params[:files].map{|f| f.original_filename}
+        filepaths = params[:files].map{|f| f.path}
+        week = params[:week]
+        @unrecognized_files = @@files_by_week[week].select{ |f| !filenames.include?(f) }
+        @result_array_disp = []
+        i = 0
+        @passed_test_cases = 0
+        @total_test_cases = 0
+        while i < filenames.length
+          filename = filenames[i]
+          filepath = filepaths[i]
+          value = `ruby -r "./E98/execute.rb" -e "Exec.run_all_test '#{filepath}', '#{week}', '#{filename}'"`
+          result = value.split("\n")
+          @result_array_disp += result
+          @passed_test_cases += value.scan(/(?=Test Case Passed)/).count
+          @total_test_cases += value.scan(/(?=Test Case)/).count / 2
+          i += 1
+        end
+        @week_name = week
     end
 
 end
